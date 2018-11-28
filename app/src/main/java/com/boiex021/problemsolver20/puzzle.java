8f2fdfeafe1ee6f09e8c5b094c898e009b06a591
BabBOIE98 <30304056+BabBOIE98@users.noreply.github.com>
2018-11-28 13:18:48 -0600

    Initial commit

---
+++ app/src/main/java/com/boiex021/problemsolver20/puzzle.java
@@ -0,0 +1,13 @@
+package com.boiex021.problemsolver20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class puzzle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle);
+    }
+}
---
+++ app/src/main/java/com/boiex021/problemsolver20/puzzleIntro.java
@@ -0,0 +1,13 @@
+package com.boiex021.problemsolver20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class puzzleIntro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle_intro);
+    }
+}
