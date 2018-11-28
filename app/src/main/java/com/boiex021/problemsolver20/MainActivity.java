bc50678021fcb597247942a9e3bfadd931d1e846
dannyBoie <30304056+BabBOIE98@users.noreply.github.com>
2018-11-28 13:44:12 -0600

    Linked Main activity to both intro activities.

--- app/src/main/java/com/boiex021/problemsolver20/MainActivity.java
+++ app/src/main/java/com/boiex021/problemsolver20/MainActivity.java
@@ -1,7 +1,9 @@
 package com.boiex021.problemsolver20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //launches fwgc introduction activity
+    public void launchFwgcIntro(View view) {
+        Intent intent = new Intent(this, fwgcIntro.class);
+        startActivity(intent);
+    }
+
+    //launches puzzle introduction activity
+    public void launchPuzzleIntro(View view) {
+        Intent intent = new Intent(this, puzzleIntro.class);
+        startActivity(intent);
+    }
+
 }
