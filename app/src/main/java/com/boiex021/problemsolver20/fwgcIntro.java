e80d77e13aa8b23dcc96687ba2382785c7b25931
dannyBoie <30304056+BabBOIE98@users.noreply.github.com>
2018-11-28 13:58:15 -0600

    Linked intro activities to problem activities

--- app/src/main/java/com/boiex021/problemsolver20/fwgcIntro.java
+++ app/src/main/java/com/boiex021/problemsolver20/fwgcIntro.java
@@ -1,7 +1,9 @@
 package com.boiex021.problemsolver20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class fwgcIntro extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class fwgcIntro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fwgc_intro);
+    }
+
+    //launches fwgc activity
+    public void launchFwgc(View view) {
+        Intent intent = new Intent(this, fwgc.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/boiex021/problemsolver20/puzzleIntro.java
+++ app/src/main/java/com/boiex021/problemsolver20/puzzleIntro.java
@@ -1,7 +1,9 @@
 package com.boiex021.problemsolver20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class puzzleIntro extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class puzzleIntro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle_intro);
+    }
+
+    //launches 8 puzzle problem
+    public void launchPuzzle(View view) {
+        Intent intent = new Intent(this, puzzle.class);
+        startActivity(intent);
     }
 }
