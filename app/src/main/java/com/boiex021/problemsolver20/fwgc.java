63cc66d9c721552420bb6e1c390f14cfc6f602a9
dannyBoie <30304056+BabBOIE98@users.noreply.github.com>
2018-12-05 13:47:26 -0600

    Buttons that change state, display error and congratulations message, and reset button all work including initial state (in fwgc.java)

--- app/src/main/java/com/boiex021/problemsolver20/fwgc.java
+++ app/src/main/java/com/boiex021/problemsolver20/fwgc.java
@@ -20,18 +20,14 @@ import framework.solution.SolvingAssistant;
 
 public class fwgc extends AppCompatActivity {
 
-    FarmerState initial = new FarmerState("West",
-                                        "West",
-                                        "West",
-                                        "West");
-
-    TextView initialText;
-
     FarmerProblem problem;
     SolvingAssistant solver;
     Mover mover;
     List<String> moveNames;
     String farmerMove;
+    TextView stateText;
+    TextView errMessage;
+    TextView congratMessage;
 
 
     @Override
@@ -39,9 +35,13 @@ public class fwgc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fwgc);
 
-        initialText.setText(initial.toString());
-
+        stateText = findViewById(R.id.textView9);
         problem = new FarmerProblem();
+        stateText.setText(problem.getInitialState().toString());
+
+        errMessage = findViewById(R.id.textView7);
+        congratMessage = findViewById(R.id.textView10);
+
         solver = new SolvingAssistant(problem);
         mover = problem.getMover();
         moveNames = mover.getMoveNames();
@@ -52,16 +52,76 @@ public class fwgc extends AppCompatActivity {
 
     //helper method
 
+
     public void farmerOnClick(View view) {
-        TextView toStringView = findViewById(R.id.textView9);
 
-        solver.tryMove(farmerMove);
-        if(solver.isMoveLegal()) {
-            toStringView.setText(problem.getCurrentState().toString());
+        State next = mover.doMove("Farmer is going alone", problem.getCurrentState());
+
+        if (next != null) {
+            problem.setCurrentState(next);
+            errMessage.setText(" ");
+            stateText.setText(next.toString());
+            if (next.equals(problem.getFinalState())) {
+                congratMessage.setText(R.string.congrats);
+            }
+        } else {
+            errMessage.setText(R.string.errorMessage);
         }
-
     }
 
+
+    public void wolfOnClick(View view) {
+        State next = mover.doMove("Farmer is taking the Wolf", problem.getCurrentState());
+
+        if (next != null) {
+            problem.setCurrentState(next);
+            errMessage.setText(" ");
+            stateText.setText(next.toString());
+            if (next.equals(problem.getFinalState())) {
+                congratMessage.setText(R.string.congrats);
+            }
+        } else {
+            errMessage.setText(R.string.errorMessage);
+        }
+    }
+
+
+    public void goatOnClick(View view) {
+        State next = mover.doMove("Farmer is taking the Goat", problem.getCurrentState());
+
+        if (next != null) {
+            problem.setCurrentState(next);
+            errMessage.setText(" ");
+            stateText.setText(next.toString());
+            if (next.equals(problem.getFinalState())) {
+                congratMessage.setText(R.string.congrats);
+            }
+        } else {
+            errMessage.setText(R.string.errorMessage);
+        }
+    }
+
+
+    public void cabbageOnClick(View view) {
+        State next = mover.doMove("Farmer is taking the Cabbage", problem.getCurrentState());
+
+        if (next != null) {
+            problem.setCurrentState(next);
+            errMessage.setText(" ");
+            stateText.setText(next.toString());
+            if (next.equals(problem.getFinalState())) {
+                congratMessage.setText(R.string.congrats);
+            }
+        } else {
+            errMessage.setText(R.string.errorMessage);
+        }
+    }
+
+    public void resetOnClick(View view) {
+        stateText.setText(problem.getInitialState().toString());
+        errMessage.setText(" ");
+        congratMessage.setText(" ");
+    }
 }
 
 
